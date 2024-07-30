76a843b6e836897f3630f8b1fd966263f0ed0825
Maha-Gamaleldin <114989552+Maha-Gamaleldin@users.noreply.github.com>
2024-07-30 09:12:38 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Maha Gamaleldeen on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
